8edc79bf03c8d320cbc52fc9a06e83eae3af7da4
Christian05github <estudillochristian18@gmail.com>
2025-08-20 08:56:45 +0800

    New ToDo Model

---
+++ Models/ToDo.cs
@@ -0,0 +1,14 @@
+﻿namespace OneHelper.Models
+{
+    public class ToDo
+    {
+        public required int ID { get; set; }
+        public required string Title { get; set; } = string.Empty;
+        public string? Description { get; set; } = string.Empty;
+        public required TimeSpan StartTime { get; set; }
+        public required DateTime EndTime { get; set; }
+        public required int PriorityLevel { get; set; }
+        public bool? IsCompleted { get; set; }
+
+    }
+}
