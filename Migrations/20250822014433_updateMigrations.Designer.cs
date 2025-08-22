7b2a2469395556d56356f0a3833d3d6b9853d279
TRAINEE-SUMAYLO <TRAINEE-SUMAYLO@FGT-08>
2025-08-22 09:46:52 +0800

    Updated migrations with files

--- Migrations/20250822014433_updateMigrations.Designer.cs
+++ Migrations/20250822014433_updateMigrations.Designer.cs
@@ -3,6 +3,7 @@ using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using OneHelper.Models;
 
@@ -11,9 +12,11 @@ using OneHelper.Models;
 namespace OneHelper.Migrations
 {
     [DbContext(typeof(OneHelperContext))]
-    partial class OneHelperContextModelSnapshot : ModelSnapshot
+    [Migration("20250822014433_updateMigrations")]
+    partial class updateMigrations
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
