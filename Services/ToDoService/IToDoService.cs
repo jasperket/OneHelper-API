3ee3c4217a9c959ef83e8ac98bee0f42149feecb
Christian05github <estudillochristian18@gmail.com>
2025-09-08 09:37:02 +0800

    added interface methods

--- Services/ToDoService/IToDoService.cs
+++ Services/ToDoService/IToDoService.cs
@@ -1,6 +1,17 @@
-﻿namespace OneHelper.Services.ToDoService
+﻿using OneHelper.Models;
+using OneHelper.Repository.Interfaces;
+
+namespace OneHelper.Services.ToDoService
 {
     public interface IToDoService
     {
+
+        public Task<IEnumerable<ToDo>> GetAllToDosAsync();
+
+        public Task<ToDo?> GetToDoByIdAsync(int id);
+
+        public Task AddToDoAsync(ToDo item);
+        public Task UpdateToDoAsync(ToDo item);
+        public Task DeleteToDoAsync(int id);
     }
 }
